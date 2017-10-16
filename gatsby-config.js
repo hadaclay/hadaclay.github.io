9606c6abab1c79ed9c556530124ac29b26d336b2
Clayton Hadaway <me@hadaclay.com>
2017-10-16 16:03:33 -0500

    Updates

--- gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Clayton Hadaway - JavaScript Developer`
-  },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-react-css-modules`],
-  pathPrefix: '/'
-};
